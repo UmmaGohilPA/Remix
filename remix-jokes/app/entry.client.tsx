3335787bdb8d63d9624baa8ce753549ec4b3177a
umma-pa <umma.gohil@paconsulting.com>
2022-01-04 17:25:27 +0000

    Restart

--- remix-jokes/app/entry.client.tsx
+++
@@ -1,8 +0,0 @@
-import { hydrate } from "react-dom";
-import { RemixBrowser } from "remix";
-
-hydrate(<RemixBrowser />, document);
-
-// This is the first bit of your JavaScript that will run when the app loads in the browser. We use this file to hydrate our React components.
-
-// https://reactjs.org/docs/react-dom.html#hydrate
