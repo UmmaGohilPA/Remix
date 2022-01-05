b44a678b0648257178a26f5e168132866f39ffd8
umma-pa <umma.gohil@paconsulting.com>
2022-01-05 11:05:07 +0000

    Error boundaries

--- remix-jokes/app/routes/logout.tsx
+++ remix-jokes/app/routes/logout.tsx
@@ -2,11 +2,10 @@ import type { ActionFunction, LoaderFunction } from "remix";
 import { redirect } from "remix";
 import { logout } from "~/utils/session.server";
 
-export const action: ActionFunction = async ({
-  request
-}) => {
+export const action: ActionFunction = async ({ request }) => {
   return logout(request);
 };
 
 export const loader: LoaderFunction = async () => {
   return redirect("/");
+};
